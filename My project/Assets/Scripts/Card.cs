efba15a41c8f84136739a6400cf37d8a5815b9f0
Lidiia <andrienkolidia@gmail.com>
2022-10-13 15:55:03 +0300

    Changing

--- My project/Assets/Scripts/Card.cs
+++ My project/Assets/Scripts/Card.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour, IPointerEnterHandler {
-    public void OnPointerEnter(PointerEventData eventData)
-    {
+public class Card : MonoBehaviour {
+    protected bool cheking;
+    void Update(){
+        if (cheking) {
 
+        }
     }
 }
