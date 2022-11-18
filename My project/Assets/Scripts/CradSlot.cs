637600162d8d3c667b9689d0697b3eef5717bf3e
Lidiia <andrienkolidia@gmail.com>
2022-11-18 10:28:10 +0200

    new changes

--- My project/Assets/Scripts/CradSlot.cs
+++ My project/Assets/Scripts/CradSlot.cs
@@ -21,6 +21,9 @@ public class CradSlot : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
         Debug.Log("OnDrop");
         if(eventData.pointerDrag != null){
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            Sprite BigCard = Resources.Load<Sprite>("Sprites/"+eventData.pointerDrag.GetComponent<Image>().sprite.name + " big");
+            eventData.pointerDrag.GetComponent<Image>().sprite = BigCard;
+            eventData.pointerDrag.GetComponent<RectTransform>().sizeDelta = new Vector2(230, 130); 
         }
     }
 
